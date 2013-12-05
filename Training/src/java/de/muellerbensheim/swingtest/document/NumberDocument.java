9685f3a2d0908a2f3ccb4d194ee77bae4ebd47d4
sascha <sascha-marcus@gmx.net>
2013-12-05 15:32:34 -0800

    Document für Zahlen

---
+++ Training/src/java/de/muellerbensheim/swingtest/document/NumberDocument.java
@@ -0,0 +1,26 @@
+package de.muellerbensheim.swingtest.document;
+
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.PlainDocument;
+
+public class NumberDocument extends PlainDocument {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void insertString(int offs, String str, AttributeSet a)
+			throws BadLocationException {
+		try {
+			Integer.parseInt(str);
+		} catch (Exception ex) {
+			return;
+		}
+
+		super.insertString(offs, str, a);
+	}
+
+}

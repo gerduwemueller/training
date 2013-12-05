3ced2bb11b5d325b42eef2afe3600ca1de3cfdc4
Gerd Müller <gerd@mueller-bensheim.de>
2013-12-05 23:09:42 +0100

    Klasse gelöscht

--- Training/src/java/de/muellerbensheim/hallowelt/NochEinHallo.java
+++
@@ -1,9 +0,0 @@
-package de.muellerbensheim.hallowelt;
-
-public class NochEinHallo {
-	public static void main(String[] args) {
-		System.out.println("Hallo Gerd");
-		System.out.println("Es ist schon spät");
-		System.out.println("Es ist später - Teil4");
-	}
-}

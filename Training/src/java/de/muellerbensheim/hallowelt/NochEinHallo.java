d43376d5da6d825b447ca79a733290d72c3309ee
sascha <sascha-marcus@gmx.net>
2013-12-05 14:03:50 -0800

    4

--- Training/src/java/de/muellerbensheim/hallowelt/NochEinHallo.java
+++ Training/src/java/de/muellerbensheim/hallowelt/NochEinHallo.java
@@ -4,6 +4,6 @@ public class NochEinHallo {
 	public static void main(String[] args) {
 		System.out.println("Hallo Gerd");
 		System.out.println("Es ist schon spät");
-		System.out.println("Es ist später - Teil3");
+		System.out.println("Es ist später - Teil4");
 	}
 }

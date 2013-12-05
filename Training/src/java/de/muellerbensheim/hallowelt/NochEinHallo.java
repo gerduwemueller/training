4ead529374c8ff057c260aa13173acd534b7b0c4
Gerd Müller <gerd@mueller-bensheim.de>
2013-12-05 22:55:04 +0100

    Es ist später

--- Training/src/java/de/muellerbensheim/hallowelt/NochEinHallo.java
+++ Training/src/java/de/muellerbensheim/hallowelt/NochEinHallo.java
@@ -4,5 +4,6 @@ public class NochEinHallo {
 	public static void main(String[] args) {
 		System.out.println("Hallo Gerd");
 		System.out.println("Es ist schon spät");
+		System.out.println("Es ist später");
 	}
 }

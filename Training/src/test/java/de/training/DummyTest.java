4998178e2d48e6040361f90d59983741900230cc
Sascha <Sascha@Sascha-PC>
2013-12-10 23:04:28 +0100

    Erster JUnit

---
+++ Training/src/test/java/de/training/DummyTest.java
@@ -0,0 +1,11 @@
+package de.training;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DummyTest {
+	@Test
+	public void dummyTest() {
+		Assert.assertEquals("Nicht gleich", "1", "1");
+	}
+}

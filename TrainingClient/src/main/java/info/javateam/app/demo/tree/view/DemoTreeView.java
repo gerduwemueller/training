a1e823cfa525fc738e7f23d5068f8efe0b0d0e17
Sascha <Sascha@Sascha-PC>
2013-12-29 21:11:53 +0100

    Event bei Select feuern

--- TrainingClient/src/main/java/info/javateam/app/demo/tree/view/DemoTreeView.java
+++ TrainingClient/src/main/java/info/javateam/app/demo/tree/view/DemoTreeView.java
@@ -1,14 +1,34 @@
 package info.javateam.app.demo.tree.view;
 
+import java.util.HashMap;
+
 import info.javateam.app.demo.tree.view.renderer.LabelTreeCellRenderer;
+import info.javateam.app.event.EventFactory;
 
 import javax.swing.JTree;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.DefaultMutableTreeNode;
+
+import org.dom4j.Element;
 
 public class DemoTreeView extends JTree {
 	private static final long serialVersionUID = 1L;
 
+	
+	
 	public DemoTreeView() {
 		setRootVisible(false);
 		setCellRenderer(new LabelTreeCellRenderer());
+		
+		getSelectionModel().addTreeSelectionListener(new TreeSelectionListener() {
+			@Override
+			public void valueChanged(TreeSelectionEvent e) {
+				DefaultMutableTreeNode node = (DefaultMutableTreeNode) DemoTreeView.this.getLastSelectedPathComponent();
+				Element element = (Element) node.getUserObject();
+				String action = element.valueOf("@action");
+				EventFactory.fireEvent(action, new HashMap<String, Object>());
+			}
+		});
 	}
 }
---
+++ TrainingClient/src/main/java/info/javateam/app/event/EventFactory.java
@@ -0,0 +1,36 @@
+package info.javateam.app.event;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class EventFactory {
+
+	private static List<EventListener> list = new ArrayList<EventListener>();
+
+	/**
+	 * @param eventListener
+	 */
+	public static void addEventListener(EventListener eventListener) {
+		list.add(eventListener);
+	}
+
+	/**
+	 * @param eventListener
+	 */
+	public static void removeEventListener(EventListener eventListener) {
+		list.remove(eventListener);
+	}
+
+	/**
+	 * @param eventName
+	 * @param arguments
+	 */
+	public static void fireEvent(String eventName, Map<String, Object> arguments) {
+		System.out.println("Event gefeuert: " + eventName);
+		for (EventListener eventListener : list) {
+			eventListener.fireEvent(eventName, arguments);
+		}
+	}
+
+}
